888a25799872b586beef40d1b2fc3017fa4c3327
Aurora Polak <51273154+loller10000@users.noreply.github.com>
2025-03-18 15:37:39 +0100

    dicking around
    Dicked a bit around, didnt really do that much

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -2,7 +2,7 @@
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private Transform previuosRoom;
+    [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
 
@@ -14,13 +14,13 @@ public class Door : MonoBehaviour
             {
                 cam.MoveToNewRoom(nextRoom);
                 nextRoom.GetComponent<Room>().ActivateRoom(true);
-                previuosRoom.GetComponent<Room>().ActivateRoom(false);
+                previousRoom.GetComponent<Room>().ActivateRoom(false);
             }
 
             else
             {
-                cam.MoveToNewRoom(previuosRoom);
-                previuosRoom.GetComponent<Room>().ActivateRoom(true);
+                cam.MoveToNewRoom(previousRoom);
+                previousRoom.GetComponent<Room>().ActivateRoom(true);
                 nextRoom.GetComponent<Room>().ActivateRoom(false);
 
             }
